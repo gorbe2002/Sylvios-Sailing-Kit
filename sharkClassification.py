3da836b5bef28f656b2af779111a9e61c23ef349
Gabriel <gorbe54@hotmail.com>
2023-11-04 16:54:27 -0400

    Updated image classification

--- sharkClassification.py
+++ sharkClassification.py
@@ -1,8 +1,38 @@
+# needed libraries:
 import matplotlib.pyplot as plt
-import numpy as np
-import PIL
 import tensorflow as tf
 
 from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
+
+# creating dataset:
+batch_size = 64
+img_height = 180
+img_width = 180
+
+train_ds = tf.keras.utils.image_dataset_from_directory(
+    "sharkPhotos",
+    validation_split=0.2,
+    subset="training",
+    seed=10,
+    image_size=(img_height, img_width),
+    batch_size=batch_size)
+
+val_ds = tf.keras.utils.image_dataset_from_directory(
+    "sharkPhotos",
+    validation_split=0.2,
+    subset="validation",
+    seed=10,
+    image_size=(img_height, img_width),
+    batch_size=batch_size)
+
+# visualize data:
+plt.figure(figsize=(10, 10))
+for images, labels in train_ds.take(1):
+    for i in range(9):
+        ax = plt.subplot(3, 3, i + 1)
+        plt.imshow(images[i].numpy().astype("uint8"))
+        plt.title(int(labels[i]))
+        plt.axis("off")
+
